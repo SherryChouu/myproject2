1c69925e7ee4bea3c52e87b155f9a2bcf3321919
SherryChouu <sherrychou0710@gmail.com>
2022-03-04 16:09:27 +0800

    scoringNG

---
+++ src/main/java/com/sherry/Scoring.java
@@ -0,0 +1,12 @@
+package com.sherry;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student jack = new Student();
+        jack.name = "Jack";
+        jack.english = 70;
+        jack.math = 88;
+
+
+    }
+}
---
+++ src/main/java/com/sherry/scoringNG.java
@@ -0,0 +1,15 @@
+package com.sherry;
+
+public class scoringNG {
+    public static void main(String[] args) {
+        String[] names = { "Sherry" , "Tammy" , "Kenny", "Wayne", "Kevin" };
+        int[] english = {90, 88, 75, 60, 70};
+        int[] math = {60, 55, 84, 75, 64};
+        for (int i = 0; i < 5; i++) {
+                System.out.println(names[i] + "\t" + english[i] + "\t" + math[i] );
+            }
+
+        }
+
+
+    }
