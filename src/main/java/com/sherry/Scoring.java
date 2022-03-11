cba276144d265050826a6f33dd5e66f50dc6a01e
SherryChouu <sherrychou0710@gmail.com>
2022-03-11 15:27:34 +0800

    sccring

--- src/main/java/com/sherry/Scoring.java
+++ src/main/java/com/sherry/Scoring.java
@@ -2,11 +2,10 @@ package com.sherry;
 
 public class Scoring {
     public static void main(String[] args) {
-        Student jack = new Student();
-        jack.name = "Jack";
-        jack.english = 70;
-        jack.math = 88;
-
+        Student jack = new Student("Jack", 70, 88);
+        Student hank = new Student("Hank",70,88);
+        jack.print();
+        hank.print();
 
     }
 }
--- src/main/java/com/sherry/Student.java
+++ src/main/java/com/sherry/Student.java
@@ -4,4 +4,19 @@ public class Student {
     String name;
     int english;
     int math;
+    public Student(String name){
+        this.name = name;
+    }
+    public Student(String name, int english, int math){
+        this.name = name;
+        this.english = english;
+        this.math = math;
+    }
+    public void print(){
+        int average = (english+math/2);
+        System.out.println(name + "/t" + english + "/t" + math + "/t");
+        if (average < 60){
+            System.out.println("*");
+        }
+    }
 }
