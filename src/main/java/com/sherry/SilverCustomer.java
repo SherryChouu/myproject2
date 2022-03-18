99f75338fd9c19106879e47e3b515ceef6fe946a
SherryChouu <sherrychou0710@gmail.com>
2022-03-18 15:52:54 +0800

    Customer

---
+++ src/main/java/com/sherry/SilverCustomer.java
@@ -0,0 +1,14 @@
+package com.sherry;
+
+public class SilverCustomer extends Customer{
+    public SilverCustomer(String id, int amount){
+        super(id , amount);
+    }
+  public int getReturnMoney(){
+        return (int)(amount*0.1f);
+  }
+  @Override
+    public void  print(){
+      System.out.println(id + "\t" + amount + "\t" + (amount-getOffMoney()) + "("+getOffMoney()+")");
+  }
+}
