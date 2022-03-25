53f13838bfe0860f32026aab3bde1f494212ece2
SherryChouu <sherrychou0710@gmail.com>
2022-03-25 19:22:27 +0800

    ticket

--- src/main/java/com/sherry/score/GraduateStudent.java
+++ src/main/java/com/sherry/score/GraduateStudent.java
@@ -4,10 +4,7 @@ import com.sherry.score.Student;
 
 public class GraduateStudent extends Student {
     int thesis;
-    public GraduateStudent(String name,
-                           int english,
-                           int math,
-                           int thesis){
+    public GraduateStudent(String name, int english, int math, int thesis){
         super(name, english, math);
         this.thesis = thesis;
     }
